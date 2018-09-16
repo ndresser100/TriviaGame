dd9a9c24c5cb41c83644d9d82d2feaf6186dd731
NateDog <n.dresser100@gmail.com>
2018-09-15 20:02:14 -0500

    updating triviagame

---
+++ assets/javascript/app.js
@@ -0,0 +1,226 @@
+$(document).ready(function () {
+	// entire javascript goes here
+	
+	// global variables
+	$('#image').css('display', 'none');
+	// timer variables
+	var timerNumber = 31;
+
+	// score variables
+	var numCorrect = 0;
+	var numIncorrect = 0;
+	var numAnswered = 0;
+
+	// question and answer variables
+	var answers = [];
+	var currentQuestion = 0;
+
+
+
+	// question object array
+	var trivia = [
+		q1 = {
+			question: "What is the name of Han Solo's ship?",
+			correct: 2,
+			multChoice: ['Ebon Hawk', 'Outrider', 'Millenium Falcon', 'Sun Crusher'],
+			image: 'assets/images/Millennium Falcon.jpg'
+		},
+		q2 = {
+			question: "What color is Luke Skywalker's lightsaber in The Empire Strikes Back?",
+			correct: 1,
+			multChoice: ['Green', 'Blue', 'Purple', 'Yellow'],
+			image: 'assets/images/Luke Skywalker lightsaber.jpg'
+		},
+		q3 = {
+			question: "Who was Obi-Wan's master?",
+			correct: 3,
+			multChoice: ['Yoda', 'Mace Windu', 'Anakin Skywalker', 'Qui-Gon Jinn'],
+			image: 'assets/images/Qui-Gon-Jinn_1.jpeg'
+		},
+		q4 = {
+			question: "What is Luke Skywalker's favorite drink?",
+			correct: 0,
+			multChoice: ['Blue Milk', 'Water', 'Dr. Pepper', 'Orange Juice'],
+			image: 'assets/images/blue-milk-star-wars.jpg'
+		},
+		q5 = {
+			question: "Whose DNA did they use to create the clones?",
+			correct: 2,
+			multChoice: ['Boba Fett', 'Obi-Wan Kenobi', 'Jango Fett', 'Anakin Skywalker'],
+			image: 'assets/images/jango.jpeg'
+		},
+		q6 = {
+			question: "Who was Count Dooku's padawan before he left the Jedi Temple?",
+			correct: 0,
+			multChoice: ['Qui-Gon Jinn', 'Obi-Wan Kenobi', 'Mace Windu', 'Yoda'],
+			image: 'assets/images/Qui-Gon-Jinn-2.webp'
+		},
+		q7 = {
+			question: "What powers a lightsaber?",
+			correct: 3,
+			multChoice: ['Amethyst', 'Diamonds', 'Quartz', 'Ilum'],
+			image: 'assets/images/Lightsaber Crystal.jpg'
+		},
+		q8 = {
+			question: "What planet is Luke Skywalker from?",
+			correct: 1,
+			multChoice: ['Endor', 'Tatooine', 'Hoth', 'Naboo'],
+			image: 'assets/images/Star-Wars-Tatooine-sunset.jpg'
+		}
+	];
+
+	// helper functions
+	//  helper function to hide html elements
+	var hide = function (elementId) {
+		$(elementId).css("visibility", "hidden");
+	};
+	// helper function to show html elements
+	var show = function (elementId) {
+		$(elementId).css("visibility", "visible");
+	};
+	// helper function for writing html elements
+	var write = function (elementId, thing) {
+		$(elementId).html('<h3>' + thing + "</h3>")
+	};
+
+	// write question function
+	var questionWrite = function () {
+		if (currentQuestion <= 7) { 
+			$('#questionDiv').html('<h2>' + trivia[currentQuestion].question + '</h2>');
+			answers = trivia[currentQuestion].multChoice;
+			show('.answer');
+			for (var i = 0; i < answers.length; i++) {
+				$('#answer' + i).html('<h3>' + answers[i] + '</h3>');
+			}
+		}
+		else {
+			gameOver();
+		}
+	};
+
+	// clears the html contents of the answers
+	var answerClear = function () {
+		// $('#answersDiv').empty();
+		for (var i = 0; i < 4; i++) {
+			$('#answer' + i).html('');
+		}
+		hide('.answer');
+	};
+
+	// Timer
+	// Timer run function
+	var start = function() {
+		// starts timer counter
+		counter = setInterval(countDown, 1000);
+
+		// clear startTitle
+		$('#startTitle').empty();
+
+		// hide start button
+		hide('#start');
+
+		//write question & answers
+		questionWrite();	
+	};
+
+	// clears all content
+	var clearScreen = function () {
+		$('#startTitle').empty();
+		$('#questionDiv').empty();
+		$('#scoreDiv').empty();
+		answerClear();
+	}
+
+	// Timer countdown function
+	var countDown = function () {
+		// decrement timerNumber
+		timerNumber --;
+		// write timer to html timerDiv
+		$('#timerDiv').html('<h2> Time Remaining: ' + timerNumber + '</h2>');
+
+		// when timer reaches 0
+		if (timerNumber == 0) {
+			gameOver();
+		}
+	};
+
+	// Timer stop function
+	var stop = function () {
+		clearInterval(counter);
+	};
+
+	// reset function
+	var reset = function () {
+		stop();
+		timerNumber = 31;
+		answers = [];
+		currentQuestion = 0;
+		clearScreen();
+		$('#timerDiv').empty();
+		write('#startTitle', 'Press Start Button to Begin!');
+		show('#start');
+		hide('#reset');
+	};
+	
+	var gameOver = function() {
+		// stop the timer
+		stop();
+
+		// clear the question and answers
+		clearScreen();
+
+		// interact with game over
+		write('#startTitle', '<h3>Game Over!</h3>');
+		$('#scoreDiv').append('<h3>Here are your results</h3>');
+		$('#scoreDiv').append('<h3>Total Questions Answered: ' + numAnswered + '</h3>');
+		$('#scoreDiv').append('<h3>Number of correct answers: ' + numCorrect + '</h3>');
+		$('#scoreDiv').append('<h3>Number of incorrect answers: ' + numIncorrect + '</h3>');
+		show('#reset');
+	};
+
+	//next question function
+	var nextQuestion = function () {
+		$('#image').css('display', 'none');
+		$('#questionDiv').css('display', 'initial');
+		$('#answersDiv').css('display', 'initial');
+		$('#answerMsg').css('display', 'none');
+		clearInterval();
+		timerNumber = 31;
+	}
+
+	//check answer
+	$('.answer').click(function () {
+		var clicked = $(this);
+		var value = clicked.attr('value');
+		var correctAnswer = trivia[currentQuestion].correct;
+
+		if (value == correctAnswer) {
+			$('#questionDiv').empty();
+			answerClear();
+			$('#answersDiv').css('display', 'none');
+			$('#questionDiv').css('display', 'none');
+			$('#answerMsg').css('display', 'initial');
+			$('#image').attr('src', trivia[currentQuestion].gif);
+			$('#image').css('display', 'initial');
+			$('#answerMsg').html('<h3> You chose ' + answers[value] + '.</h3> <br><h3>The correct answer was ' + answers[correctAnswer] + '.</h3>');
+			setInterval(nextQuestion, 5 * 1000);
+			numAnswered ++;
+			numCorrect ++;
+			currentQuestion ++;
+			questionWrite();
+		}
+		else {
+			numAnswered ++;
+			numIncorrect ++;
+			currentQuestion ++;
+			timerNumber = 31;
+			$('#questionDiv').empty();
+			answerClear();
+			questionWrite();
+		}
+	});
+
+	 // click handlers	
+	$('#start').on("click", start);
+	$('#reset').on('click', reset);
+})
